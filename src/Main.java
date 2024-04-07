2e6bd63b3781a65911e6b102fdd52f5a6d5a703d
Long Nguyen <longnguyen11898@gmail.com>
2024-04-07 16:30:13 +0700

    Finish Day 7 Practice 2

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student("Long", 25, "Hanoi");
+        Student student2 = new Student("Khang", 20, "Ninh Binh");
+        Student student3 = new Student("Mai", 19, "Bac Ninh");
+
+        Map<Integer, Student> studentMap = new HashMap<Integer, Student>();
+        studentMap.put(1, student1);
+        studentMap.put(2, student2);
+        studentMap.put(3, student3);
+        studentMap.put(4, student1);
+
+        for (Map.Entry<Integer, Student> student : studentMap.entrySet()){
+            System.out.println(student.toString());
+        }
+
+        System.out.println("...............Set");
+        Set<Student> students = new HashSet<Student>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student1);
+
+        for (Student student: students){
+            System.out.println(student.toString());
+        }
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    private String name;
+    private int age;
+    private String address;
+
+    public Student() {
+    }
+
+    public Student(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student@name = " + name + ", age = " + age + ", and address = " + address;
+    }
+}
